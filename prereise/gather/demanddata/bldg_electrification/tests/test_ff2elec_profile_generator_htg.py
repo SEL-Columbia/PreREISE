4b206e1c6e7bcbf3822a0e99f5ee1d1b93457061
yinboHU <yh3184@columbia.edu>
2023-01-31 11:20:39 -0500

    style

--- prereise/gather/demanddata/bldg_electrification/tests/test_ff2elec_profile_generator_htg.py
+++ prereise/gather/demanddata/bldg_electrification/tests/test_ff2elec_profile_generator_htg.py
@@ -7,7 +7,11 @@ from prereise.gather.demanddata.bldg_electrification.ff2elec_profile_generator_h
 
 
 def test_generate_profiles_argument_type():
-    args = (("2016", const.state_list, "res", "advperfhp"), (2016, const.state_list, 3, "advperfhp"), (2016, const.state_list, "com", 3))
+    args = (
+        ("2016", const.state_list, "res", "advperfhp"),
+        (2016, const.state_list, 3, "advperfhp"),
+        (2016, const.state_list, "com", 3),
+    )
     for a in args:
         with pytest.raises(TypeError):
             generate_htg_profiles(a[0], a[1], a[2])
